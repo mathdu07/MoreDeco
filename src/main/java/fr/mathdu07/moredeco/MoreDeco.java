2f73d0912c5fd72261a7d409316b222603002558
mathdu07 <mathdu07.minecraft@gmail.com>
2014-09-01 21:46:51 +0200

    Update to 0.2

--- src/main/java/fr/mathdu07/moredeco/MoreDeco.java
+++ src/main/java/fr/mathdu07/moredeco/MoreDeco.java
@@ -22,7 +22,7 @@ public class MoreDeco
 {
     public static final String MODID = "moredeco";
     public static final String NAME = "More Deco";
-    public static final String VERSION = "0.2-SNAPSHOT";
+    public static final String VERSION = "0.2";
     
     @Instance(MoreDeco.MODID)
     private static MoreDeco instance;
